28e279a17ed3badef0fee9b362ef4b950a6dab93
Uzytkownik <uzytkownik@kursGIT.pl>
2022-10-26 20:24:24 +0200

    workshop1 zadanie 1

--- jdbc/src/main/java/pl/sdacademy/java/jdbc/workshop2/Workshop2.java
+++ jdbc/src/main/java/pl/sdacademy/java/jdbc/workshop2/Workshop2.java
@@ -2,6 +2,8 @@ package pl.sdacademy.java.jdbc.workshop2;
 
 import pl.sdacademy.java.jdbc.utils.ApplicationPropertiesProvider;
 
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,19 +12,33 @@ import java.util.Scanner;
     * Co się stanie, jeśli użytkownik poda tekst `Narnia ' OR 'x' = 'x`?
     * Jak można temu zapobiec?
  */
-public class Workshop2 {
 
-    public static void main(String[] args) {
-        final String jdbcUrl = ApplicationPropertiesProvider.getApplicationProperties().getProperty("jdbc.url");
 
-        System.out.println("Podaj nazwę kraju z którego załadować miasta:");
-        final var countryName = new Scanner(System.in).nextLine();
+    public class Workshop2 {
 
-        final var cities = getCities(jdbcUrl, countryName);
-        System.out.printf("Miasta w '%s':\n\n%s", countryName, String.join("\n", cities));
+        public static void main(String[] args) {
+            final String jdbcUrl = ApplicationPropertiesProvider.getApplicationProperties().getProperty("jdbc.url");
+
+            System.out.println("Podaj nazwę kraju z którego załadować miasta:");
+            final var countryName = new Scanner(System.in).nextLine();
+
+            final var cities = getCities(jdbcUrl, countryName);
+            System.out.printf("Miasta w '%s':\n\n%s", countryName, String.join("\n", cities));
+        }
+
+        public static List<String> getCities(String jdbcUrl, String countryName) {
+            final List<String> cities = new LinkedList<>();
+            try (final Connection connection = DriverManager.getConnection(jdbcUrl)) {
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT city FROM city JOIN country ON country.country_id = city.country_id WHERE country = ?");
+                preparedStatement.setString(1, countryName);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    cities.add(resultSet.getString("city"));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return cities;
+        }
     }
-
-    public static List<String> getCities(String jdbcUrl, String countryName) {
-        throw new UnsupportedOperationException("TODO");
-    }
-}
